09ba4e67e140d05605b0a6302f1e53f50e7b3739
Sashko Stubailo <sashko@stubailo.com>
2015-11-04 11:32:59 -0800

    Rename util to imports

---
+++ packages/less-imports/package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'less-imports',
+  summary: 'LESS variables and mixins used throughout the app',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('less');
+
+  api.addFiles([
+    'reset.less',
+    'imports.import.less',
+    'imports/helpers.import.less',
+    'imports/lesshat.import.less',
+    'imports/fontface.import.less',
+    'imports/text.import.less',
+    'imports/typography.import.less',
+    'imports/variables.import.less',
+  ]);
+});
--- packages/todos-lib/package.js
+++ packages/todos-lib/package.js
@@ -22,6 +22,6 @@ Package.onUse(function(api) {
     'reactive-dict',
     'underscore',
     'touchwipe',
-    'less-util',
+    'less-imports',
   ]);
 });
