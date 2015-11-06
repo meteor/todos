73436a5dd23aa9e291c065b0c03dbc5c169e843f
Sashko Stubailo <sashko@stubailo.com>
2015-11-06 14:14:03 -0800

    Set up forgot password

--- packages/todos-auth/auth.js
+++ packages/todos-auth/auth.js
@@ -1,11 +1,13 @@
 /* global AccountsTemplates */
 
 Template['override-atPwdFormBtn'].replaces('atPwdFormBtn');
+Template['override-atPwdForm'].replaces('atPwdForm');
 Template['override-atTextInput'].replaces('atTextInput');
 Template['override-atTitle'].replaces('atTitle');
 Template['override-atError'].replaces('atError');
 
 AccountsTemplates.configure({
+  showForgotPasswordLink: true,
   texts: {
     errors: {
       loginForbidden: "Incorrect username or password",
