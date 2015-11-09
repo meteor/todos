1caba6e5f9ad9645b8ef7cf7c3c7c0dea150ec60
Tom Coleman <tom@thesnail.org>
2015-11-09 16:42:13 +1100

    Cleaning up a bit

--- packages/lists-show/lists-show-page.js
+++ packages/lists-show/lists-show-page.js
@@ -18,6 +18,10 @@ Template.listsShowPage.onRendered(function() {
 });
 
 Template.listsShowPage.helpers({
+  // We use #each on an array of one item so that the "list" template is
+  // removed and a new copy is added when changing lists, which is
+  // important for animation purposes. #each looks at the _id property of it's
+  // items to know when to insert a new item and when to update an old one.
   listIdArray() {
     const instance = Template.instance();
     return [{_id: instance.state.get('listId')}];
--- packages/todos-app/app-body.js
+++ packages/todos-app/app-body.js
@@ -30,13 +30,6 @@ Template.appBody.onCreated(function() {
 });
 
 Template.appBody.helpers({
-  // We use #each on an array of one item so that the "list" template is
-  // removed and a new copy is added when changing lists, which is
-  // important for animation purposes. #each looks at the _id property of it's
-  // items to know when to insert a new item and when to update an old one.
-  thisArray() {
-    return [this];
-  },
   menuOpen() {
     const instance = Template.instance();
     return instance.state.get('menuOpen') && 'menu-open';
