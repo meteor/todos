d70c9c05d511df85aa45452be3423e76567c1e78
Sashko Stubailo <sashko@stubailo.com>
2015-11-10 10:18:55 -0800

    Style the message/result

--- packages/todos-auth/accounts-templates.js
+++ packages/todos-auth/accounts-templates.js
@@ -1,5 +1,7 @@
 /* global AccountsTemplates */
 
+// We identified the templates that need to be overridden by looking at the available templates
+// here: https://github.com/meteor-useraccounts/unstyled/tree/master/lib
 Template['override-atPwdFormBtn'].replaces('atPwdFormBtn');
 Template['override-atPwdForm'].replaces('atPwdForm');
 Template['override-atTextInput'].replaces('atTextInput');
