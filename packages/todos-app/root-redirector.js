521005772c69cc269fd399839b04b9b8471acefa
Tom Coleman <tom@thesnail.org>
2015-11-11 13:50:53 +1100

    Fix issue when removing lists
    cc @arunoda

--- packages/todos-app/root-redirector.js
+++ packages/todos-app/root-redirector.js
@@ -1,5 +1,9 @@
 /* globals FlowRouter, Lists */
 
 Template.rootRedirector.onCreated(() => {
-  FlowRouter.go('listsShow', Lists.findOne());
+  // We need to set a timeout here so that we don't redirect from inside a redirection
+  //   which is a no-no in FR. TODO: find out from arunoda if this is correct.
+  Meteor.setTimeout(() => {
+    FlowRouter.go('listsShow', Lists.findOne());
+  });
 });
