bf56eed4c85869196c657f591fe339cc01c620d9
Sashko Stubailo <sashko@stubailo.com>
2015-11-12 01:13:27 -0800

    Secure publication

--- packages/todos/publications.js
+++ packages/todos/publications.js
@@ -4,5 +4,20 @@
 Meteor.publish('list/todos', function(listId) {
   check(listId, String);
 
-  return Todos.find({listId: listId});
+  const publicCondition = {
+    listId: listId,
+    userId: { $exists: false }
+  };
+
+  const privateCondition = {
+    listId: listId,
+    userId: this.userId
+  };
+
+  // We need to make sure that you can only get todos that are in a public list, or in a list that
+  // belongs to the current user, so we need to use Mongo $or.
+  return Todos.find({ $or: [
+    publicCondition,
+    privateCondition
+  ]});
 });
