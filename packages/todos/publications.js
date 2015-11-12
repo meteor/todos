3a088d1d4747f7200f0dc9bd04d208f40b6a6406
Sashko Stubailo <sashko@stubailo.com>
2015-11-12 01:14:47 -0800

    Make logic better

--- packages/todos/publications.js
+++ packages/todos/publications.js
@@ -4,12 +4,17 @@
 Meteor.publish('list/todos', function(listId) {
   check(listId, String);
 
-  const publicCondition = {
+  const publicTodosSelector = {
     listId: listId,
     userId: { $exists: false }
   };
 
-  const privateCondition = {
+  if (! this.userId) {
+    // If we aren't logged in, only return public todo items
+    return Todos.find(publicTodosSelector);
+  }
+
+  const privateTodosSelector = {
     listId: listId,
     userId: this.userId
   };
@@ -17,7 +22,7 @@ Meteor.publish('list/todos', function(listId) {
   // We need to make sure that you can only get todos that are in a public list, or in a list that
   // belongs to the current user, so we need to use Mongo $or.
   return Todos.find({ $or: [
-    publicCondition,
-    privateCondition
+    publicTodosSelector,
+    privateTodosSelector
   ]});
 });
