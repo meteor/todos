f9ac873699819268b103ab4deaddc5bd34972aab
Tom Coleman <tom@thesnail.org>
2015-11-23 12:52:17 +1100

    Need this

--- packages/lists/lists.js
+++ packages/lists/lists.js
@@ -45,7 +45,7 @@ Lists.userIdDenormalizer = userIdDenormalizer;
 Lists.schema = new SimpleSchema({
   name: { type: String },
   incompleteCount: {type: Number, defaultValue: 0},
-  userId: { type: String, optional: true }
+  userId: { type: String, regEx: SimpleSchema.RegEx.Id, optional: true }
 });
 
 Lists.attachSchema(Lists.schema);
