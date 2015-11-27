25a1d31f5dfb94cb1061120d7ca7e662065bed7e
Tom Coleman <tom@thesnail.org>
2015-11-27 16:21:55 +1100

    This is a bit simpler

--- packages/lists-show/lists-show-page.js
+++ packages/lists-show/lists-show-page.js
@@ -22,12 +22,9 @@ Template.listsShowPage.helpers({
   // removed and a new copy is added when changing lists, which is
   // important for animation purposes. #each looks at the _id property of it's
   // items to know when to insert a new item and when to update an old one.
-  listIdArray() {
+  listArray() {
     const instance = Template.instance();
-    return [{_id: instance.state.get('listId')}];
-  },
-  list() {
-    const instance = Template.instance();
-    return Lists.findOne(instance.state.get('listId'));
+    const list = Lists.findOne(instance.state.get('listId'));
+    return [list];
   }
 });
