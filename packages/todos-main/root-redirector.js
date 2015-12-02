6495d1ef9eeb78bffa409f6758a07b94ff6195a3
Tom Coleman <tom@thesnail.org>
2015-12-02 16:51:56 +1100

    I did find out, this is the way to do it.

--- packages/todos-main/root-redirector.js
+++ packages/todos-main/root-redirector.js
@@ -2,7 +2,7 @@
 
 Template.rootRedirector.onCreated(() => {
   // We need to set a timeout here so that we don't redirect from inside a redirection
-  //   which is a no-no in FR. TODO: find out from arunoda if this is correct.
+  //   which is a no-no in FR.
   Meteor.setTimeout(() => {
     FlowRouter.go('listsShow', Lists.findOne());
   });
