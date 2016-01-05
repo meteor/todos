029bace78edd81759d6d490d1a0249ac7350acd3
Sashko Stubailo <sashko@stubailo.com>
2016-01-04 20:34:18 -0800

    Reorg

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,5 @@
-/* global Lists Todos */
+import Lists from '../api/lists/lists.js';
+import Todos from '../api/todos/todos.js';
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
