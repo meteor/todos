8adbee7d1ec2a713399a1d65c9399599af0e9517
Tom Coleman <tom@thesnail.org>
2016-02-10 14:11:13 +1100

    Throw some comments in the import file

--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
+// This defines a starting set of data to be loaded if the app is loaded with an empty db.
 import '../imports/server/initializers/fixtures.js';
+
+// This file configures the Accounts package to define the UI of the reset password email.
 import '../imports/server/initializers/reset-password-email.js';
+
+// Set up some rate limiting and other important security settings.
 import '../imports/server/initializers/security.js';
 
+// This defines all the collections, publications and methods that the application provides
+// as an API to the client.
 import '../imports/api/api.js';
