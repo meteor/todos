92ec9f47d4855c5793f53123b231bf04a2bb319c
Avital Oliver <avital@thewe.net>
2016-02-16 14:33:43 -0800

    No need to explicitly import test files

--- client/tests.js
+++
@@ -1,2 +0,0 @@
-import '../imports/client/components/lists-show.tests.js';
-import '../imports/client/components/todos-item.tests.js';
--- server/tests.js
+++
@@ -1,2 +0,0 @@
-import '../imports/api/todos/todos.tests.js';
-import '../imports/api/lists/lists.tests.js';
