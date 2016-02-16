6fca4788eb7dcd054ee628e72e5681afd83cf8b1
Tom Coleman <tom@thesnail.org>
2016-02-16 13:45:02 +1100

    Spurious log

--- imports/ui/lib/errors.js
+++ imports/ui/lib/errors.js
@@ -1,7 +1,6 @@
 import { TAPi18n } from 'meteor/tap:i18n';
 
 export const displayError = (error) =>  {
-  console.log(error)
   if (error) {
     // It would be better to not alert the error here but inform the user in some
     // more subtle way
