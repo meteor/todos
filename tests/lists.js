2a2acfa5f3f4a0dcc0beac47d95a68b6d172a3e0
Tom Coleman <tom@thesnail.org>
2016-02-17 16:04:32 +1100

    Upgraded chimp to allow ES6

--- tests/lists.js
+++ tests/lists.js
@@ -1,17 +1,17 @@
 // E2E tests for creating, editing and removing lists
 
-function countLists() {
+const countLists = () => {
   browser.waitForExist('.list-todo');
   var elements = browser.elements('.list-todo');
   return elements.value.length;
 }
 
-describe('list ui', function () {
-  beforeEach(function() {
+describe('list ui', () => {
+  beforeEach(() => {
     browser.url('http://localhost:3000');
   });
 
-  it('can create a list @watch', function() {
+  it('can create a list @watch', () => {
     var initialCount = countLists();
     browser.click('.js-new-list');
 
