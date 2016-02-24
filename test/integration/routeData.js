5f558744d966b77bc94b662639cb25169356748d
Tom Coleman <tom@thesnail.org>
2016-02-24 16:47:46 +1100

    Route-based integration tests
    Requires:
    
     - https://github.com/meteor/meteor/tree/release-1.3-fix-accounts-email-message
     - https://github.com/tmeasday/test-reporter-helpers/pull/1/files

---
+++ test/integration/routeData.js
@@ -0,0 +1,29 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { generateData } from './generateData.js';
+import { Lists } from '../../imports/api/lists/lists.js';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+if (Meteor.isClient) {
+  describe('data available when routed', () => {
+    beforeEach(done => {
+      generateData()
+        .then(() => FlowRouter.go('/'))
+        .nodeify(done);
+    });
+
+    describe('when logged out', () => {
+      it('has all public lists at homepage', () => {
+        assert.equal(Lists.find().count(), 3);
+      });
+
+      it('renders the correct list when routed to', () => {
+        const list = Lists.findOne();
+        FlowRouter.go('Lists.show', {listId: list._id});
+        assert.equal($('.title-wrapper').html(), list.name);
+      });
+    });
+  });
+}
