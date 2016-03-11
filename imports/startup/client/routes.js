366ebc850c28bac5cf68b36b37831307a552360e
Tom Coleman <tom@thesnail.org>
2016-03-11 11:16:01 +1100

    The router should be on in full-app test mode
    I think this was a merging problem

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,9 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
-if (Meteor.isTest) {
-  return;
-}
-
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { AccountsTemplates } from 'meteor/useraccounts:core';
