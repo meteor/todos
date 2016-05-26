599b55d91a0dbedf16bdf26a7aa063936c51d240
Michael Shilman <shilman@lab80.co>
2016-05-26 16:41:17 +0900

    Fix linting errors

--- imports/ui/components/TodoItem.tests.js
+++ imports/ui/components/TodoItem.tests.js
@@ -7,7 +7,6 @@ import { shallow, mount } from 'enzyme';
 import { chai } from 'meteor/practicalmeteor:chai';
 import { sinon } from 'meteor/practicalmeteor:sinon';
 import TodoItem from './TodoItem.jsx';
-import { _ } from 'meteor/underscore';
 
 if (Meteor.isClient) {
   describe('TodoItem', () => {
@@ -18,22 +17,22 @@ if (Meteor.isClient) {
       chai.assert.equal(item.hasClass('checked'), true);
       chai.assert.equal(item.find('input[type="text"]').prop('defaultValue'), 'testing');
     });
-    describe('interaction', function() {
-      beforeEach(function() {
+    describe('interaction', function testInteraction() {
+      beforeEach(() => {
         const todo = Factory.create('todo', { text: 'testing' });
         this.item = mount(<TodoItem todo={todo} />);
       });
-      it('should update text when edited', function () {
+      it('should update text when edited', () => {
         const stub = sinon.stub(this.item.instance(), 'throttledUpdate');
-        this.item.find('input[type="text"]').simulate('change', {target: {value: 'tested'}});
+        this.item.find('input[type="text"]').simulate('change', { target: { value: 'tested' } });
         chai.assert(stub.calledWith('tested'));
       });
-      it('should update status when checked', function () {
+      it('should update status when checked', () => {
         const stub = sinon.stub(this.item.instance(), 'setTodoCheckStatus');
         this.item.find('input[type="checkbox"]').simulate('change', { target: { checked: true } });
         chai.assert(stub.calledOnce);
       });
-      it('should delete when trash is clicked', function () {
+      it('should delete when trash is clicked', () => {
         const stub = sinon.stub(this.item.instance(), 'deleteTodo');
         this.item.find('.delete-item').simulate('click');
         chai.assert(stub.calledOnce);
