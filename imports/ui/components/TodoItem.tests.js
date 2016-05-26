f730c3e7e71e23bd4de5053d970a14f5f11e2530
Michael Shilman <shilman@lab80.co>
2016-05-26 15:32:30 +0900

    Unit test for text editing

--- imports/ui/components/TodoItem.tests.js
+++ imports/ui/components/TodoItem.tests.js
@@ -3,9 +3,11 @@
 import { Meteor } from 'meteor/meteor';
 import { Factory } from 'meteor/factory';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { chai } from 'meteor/practicalmeteor:chai';
+import { sinon } from 'meteor/practicalmeteor:sinon';
 import TodoItem from './TodoItem.jsx';
+import { _ } from 'meteor/underscore';
 
 if (Meteor.isClient) {
   describe('TodoItem', () => {
@@ -15,6 +17,13 @@ if (Meteor.isClient) {
       chai.assert.equal(item.hasClass('list-item'), true);
       chai.assert.equal(item.hasClass('checked'), true);
       chai.assert.equal(item.find('input[type="text"]').prop('defaultValue'), 'testing');
+    });
+    it('should update text when edited', () => {
+      const todo = Factory.create('todo', { text: 'testing' });
+      const item = mount(<TodoItem todo={todo} />);
+      const stub = sinon.stub(item.instance(), 'throttledUpdate');
+      item.find('input[type="text"]').simulate('change', {target: {value: 'tested'}});
+      chai.assert(stub.calledWith('tested'));
     });
   });
 }
