b655978bb9e0f96b276a83571d6b994a82e01a24
Mack Browne <mackbrowne@gmail.com>
2017-05-02 09:48:31 -0400

    add bootstrap 4

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
