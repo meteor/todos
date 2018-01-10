d9b1e51a911b5782603d8ebe58998b891bede1c1
Hugh Willson <hugh@octonary.com>
2018-01-09 21:21:10 -0500

    Disable build and dependency caching

--- .testing/cache_build_and_dependencies.sh
+++ .testing/cache_build_and_dependencies.sh
@@ -1,3 +1,3 @@
 export MONGO_URL="mongodb://localhost:27017/cache"
 echo "Running meteor to cache it …"
-node ./.testing/cache_build_and_dependencies.js
+#node ./.testing/cache_build_and_dependencies.js
