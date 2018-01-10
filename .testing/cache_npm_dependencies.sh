fb3272cb586826a711c77093c8ec1d38873eb74a
Hugh Willson <hugh@octonary.com>
2018-01-09 20:27:02 -0500

    Update Node version

--- .testing/cache_npm_dependencies.sh
+++ .testing/cache_npm_dependencies.sh
@@ -1,4 +1,4 @@
 # Cache npm deps
-if [ ! -e /home/ubuntu/nvm/versions/node/v4.6.2/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp; fi
-if [ ! -e /home/ubuntu/nvm/versions/node/v4.6.2/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
+if [ ! -e /home/ubuntu/nvm/versions/node/v8.9.3/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp; fi
+if [ ! -e /home/ubuntu/nvm/versions/node/v8.9.3/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
 npm install
