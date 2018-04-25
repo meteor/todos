c828ccea65f2fb1297a20b988b02e71b632a966a
Hugh Willson <hugh@octonary.com>
2018-04-25 15:46:17 -0400

    Remove un-used `Fragment`

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
