6a52fda291a651140bc907307cc5278f56185dcc
Corentin Volant <corentinvolant@ntymail.com>
2019-10-30 15:59:18 +0800

    Get menuOpen through useMenuOpen in AuthPage

--- imports/ui/pages/AuthPage.jsx
+++ imports/ui/pages/AuthPage.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MobileMenu from '../components/MobileMenu.jsx';
+import { useMenuOpen } from '../state/MenuOpenState.jsx';
 
 // a common layout wrapper for auth pages
-const AuthPage = ({ content, link, menuOpen }) => (
-  <div className="page auth">
-    <nav>
-      <MobileMenu menuOpen={menuOpen} />
-    </nav>
-    <div className="content-scrollable">
-      {content}
-      {link}
-    </div>
-  </div>
-);
+const AuthPage = ({ content, link }) => {
+  const [menuOpen] = useMenuOpen();
 
+  return (
+    <div className="page auth">
+      <nav>
+        <MobileMenu menuOpen={menuOpen} />
+      </nav>
+      <div className="content-scrollable">
+        {content}
+        {link}
+      </div>
+    </div>
+  );
+}
 AuthPage.propTypes = {
   content: PropTypes.element.isRequired,
   link: PropTypes.element.isRequired,
-  menuOpen: PropTypes.object.isRequired,
 };
 
 export default AuthPage;
