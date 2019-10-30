91600884547eb583cd755cbceb5e6d7af16ca832
Corentin Volant <corentinvolant@ntymail.com>
2019-10-30 14:51:06 +0800

    Clean up NotFoundPage
    Remove unnecessary imports, remove transitional menuOpen

--- imports/ui/pages/NotFoundPage.jsx
+++ imports/ui/pages/NotFoundPage.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import i18n from 'meteor/universe:i18n';
-import BaseComponent from '../components/BaseComponent.jsx';
 import MobileMenu from '../components/MobileMenu.jsx';
 import Message from '../components/Message.jsx';
-import { useMenuOpen } from '../state/MenuOpenState.jsx';
 
-const NotFoundPage = () => {
-  const [menuOpen, setMenuOpen] = useMenuOpen();
-  const transitionMenuOpen = {
-    get: () => menuOpen,
-    set: (newMenuOpen) => setMenuOpen(newMenuOpen),
-  };
-  return (
-    <div className="page not-found">
-      <nav>
-        <MobileMenu menuOpen={transitionMenuOpen} />
-      </nav>
-      <div className="content-scrollable">
-        <Message title={i18n.__('pages.notFoundPage.pageNotFound')} />
-      </div>
+export const NotFoundPage = () => (
+  <div className="page not-found">
+    <nav>
+      <MobileMenu />
+    </nav>
+    <div className="content-scrollable">
+      <Message title={i18n.__('pages.notFoundPage.pageNotFound')} />
     </div>
-  );
-};
+  </div>
+);
 
 export default NotFoundPage;
