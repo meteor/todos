3dcceb87d73e1988285294f546d4106f52e2a5d6
Corentin Volant <corentinvolant@ntymail.com>
2019-10-30 15:34:17 +0800

    Transform JoinPage in FC
    I18n is broken on this page and NotFound, since inherited BaseComponent was in charge of it

--- imports/ui/pages/AuthPageJoin.jsx
+++ imports/ui/pages/AuthPageJoin.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Accounts } from 'meteor/accounts-base';
 import i18n from 'meteor/universe:i18n';
-import BaseComponent from '../components/BaseComponent.jsx';
 
 import AuthPage from './AuthPage.jsx';
+import { useMenuOpen } from '../state/MenuOpenState.jsx';
 
-class JoinPage extends BaseComponent {
-  constructor(props) {
-    super(props);
-    this.state = Object.assign(this.state, { errors: {} });
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+export const JoinPage = () => {
+  const [menuOpen] = useMenuOpen();
+  const [errors, setErrors] = useState({});
+  const [redirectTo, setRedirectTo] = useState(null);
 
-  onSubmit(event) {
+  const onSubmit = (event) => {
     event.preventDefault();
     const email = this.email.value;
     const password = this.password.value;
@@ -31,7 +28,7 @@ class JoinPage extends BaseComponent {
       errors.confirm = i18n.__('pages.authPageJoin.passwordConfirm');
     }
 
-    this.setState({ errors });
+    setErrors(errors);
     if (Object.keys(errors).length) {
       return;
     }
@@ -41,89 +38,81 @@ class JoinPage extends BaseComponent {
       password,
     }, (err) => {
       if (err) {
-        this.setState({
-          errors: { none: err.reason },
-        });
+        setErrors({ none: err.reason });
       }
-      this.redirectTo('/');
+      setRedirectTo('/');
     });
   }
 
-  render() {
-    const { errors } = this.state;
-    const errorMessages = Object.keys(errors).map(key => errors[key]);
-    const errorClass = key => errors[key] && 'error';
+  const errorMessages = Object.values(errors);
+  const errorClass = key => errors[key] && 'error';
 
-    const content = (
-      <div className="wrapper-auth">
-        <h1 className="title-auth">
-          {i18n.__('pages.authPageJoin.join')}
-        </h1>
-        <p className="subtitle-auth">
-          {i18n.__('pages.authPageJoin.joinReason')}
-        </p>
-        <form onSubmit={this.onSubmit}>
-          <div className="list-errors">
-            {errorMessages.map(msg => (
-              <div className="list-item" key={msg}>{msg}</div>
-            ))}
-          </div>
-          <div className={`input-symbol ${errorClass('email')}`}>
-            <input
-              type="email"
-              name="email"
-              ref={(c) => { this.email = c; }}
-              placeholder={i18n.__('pages.authPageJoin.yourEmail')}
-            />
-            <span
-              className="icon-email"
-              title={i18n.__('pages.authPageJoin.yourEmail')}
-            />
-          </div>
-          <div className={`input-symbol ${errorClass('password')}`}>
-            <input
-              type="password"
-              name="password"
-              ref={(c) => { this.password = c; }}
-              placeholder={i18n.__('pages.authPageJoin.password')}
-            />
-            <span
-              className="icon-lock"
-              title={i18n.__('pages.authPageJoin.password')}
-            />
-          </div>
-          <div className={`input-symbol ${errorClass('confirm')}`}>
-            <input
-              type="password"
-              name="confirm"
-              ref={(c) => { this.confirm = c; }}
-              placeholder={i18n.__('pages.authPageJoin.confirmPassword')}
-            />
-            <span
-              className="icon-lock"
-              title={i18n.__('pages.authPageJoin.confirmPassword')}
-            />
-          </div>
-          <button type="submit" className="btn-primary">
-            {i18n.__('pages.authPageJoin.joinNow')}
-          </button>
-        </form>
-      </div>
-    );
+  const content = (
+    <div className="wrapper-auth">
+      <h1 className="title-auth">
+        {i18n.__('pages.authPageJoin.join')}
+      </h1>
+      <p className="subtitle-auth">
+        {i18n.__('pages.authPageJoin.joinReason')}
+      </p>
+      <form onSubmit={onSubmit}>
+        <div className="list-errors">
+          {errorMessages.map(msg => (
+            <div className="list-item" key={msg}>{msg}</div>
+          ))}
+        </div>
+        <div className={`input-symbol ${errorClass('email')}`}>
+          <input
+            type="email"
+            name="email"
+            ref={(c) => { this.email = c; }}
+            placeholder={i18n.__('pages.authPageJoin.yourEmail')}
+          />
+          <span
+            className="icon-email"
+            title={i18n.__('pages.authPageJoin.yourEmail')}
+          />
+        </div>
+        <div className={`input-symbol ${errorClass('password')}`}>
+          <input
+            type="password"
+            name="password"
+            ref={(c) => { this.password = c; }}
+            placeholder={i18n.__('pages.authPageJoin.password')}
+          />
+          <span
+            className="icon-lock"
+            title={i18n.__('pages.authPageJoin.password')}
+          />
+        </div>
+        <div className={`input-symbol ${errorClass('confirm')}`}>
+          <input
+            type="password"
+            name="confirm"
+            ref={(c) => { this.confirm = c; }}
+            placeholder={i18n.__('pages.authPageJoin.confirmPassword')}
+          />
+          <span
+            className="icon-lock"
+            title={i18n.__('pages.authPageJoin.confirmPassword')}
+          />
+        </div>
+        <button type="submit" className="btn-primary">
+          {i18n.__('pages.authPageJoin.joinNow')}
+        </button>
+      </form>
+    </div>
+  );
 
-    const link = (
-      <Link to="/signin" className="link-auth-alt">
-        {i18n.__('pages.authPageJoin.haveAccountSignIn')}
-      </Link>
-    );
+  const link = (
+    <Link to="/signin" className="link-auth-alt">
+      {i18n.__('pages.authPageJoin.haveAccountSignIn')}
+    </Link>
+  );
 
-    return this.renderRedirect() ||
-      <AuthPage content={content} link={link} menuOpen={this.props.menuOpen} />;
-  }
-}
-
-JoinPage.propTypes = {
-  menuOpen: PropTypes.object.isRequired,
+  return redirectTo
+    ? <Redirect to={redirectTo} />
+    : <AuthPage content={content} link={link} menuOpen={menuOpen} />;
 };
 
 export default JoinPage;
