78d1a60852083f5fa909a6067e57c098cfdf532a
Corentin Volant <corentinvolant@ntymail.com>
2019-10-31 11:21:28 +0800

    Transform Loading in FC

--- imports/ui/components/Loading.jsx
+++ imports/ui/components/Loading.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import i18n from 'meteor/universe:i18n';
-import BaseComponent from './BaseComponent.jsx';
 
-class Loading extends BaseComponent {
-  render() {
-    return (
-      <img
-        src="/logo-todos.svg"
-        className="loading-app"
-        alt={i18n.__('components.loading.loading')}
-      />
-    );
-  }
-}
+const Loading = () => (
+  <img
+    src="/logo-todos.svg"
+    className="loading-app"
+    alt={i18n.__('components.loading.loading')}
+  />
+);
 
 export default Loading;
